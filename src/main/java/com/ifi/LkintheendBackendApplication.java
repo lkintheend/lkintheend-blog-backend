3428814ef882278d358ef45af91994cfe0e426b4
lkintheend <lkintheend@gmail.com>
2018-10-01 16:35:50 +0700

    fist commit

---
+++ src/main/java/com/ifi/LkintheendBackendApplication.java
@@ -0,0 +1,12 @@
+package com.ifi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LkintheendBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LkintheendBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ifi/LkintheendBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ifi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LkintheendBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
